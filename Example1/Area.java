6d450e626a2d4753fcecd0ab3d20a4545e9ead8c
Sana Ajani <sanaajani@msn.com>
2019-04-26 09:07:15 -0700

    example one

---
+++ Example1/Area.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Area {
+
+    //calculate the area of a rectangle based on the height and width given by the user
+    
+    public static void main(String[] args) {
+        int width;
+        int height;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a width");
+        width = input.nextInt();
+        System.out.println("Please enter a height");
+        height = input.nextInt();
+
+        int area = ;// fill in the area formula and print it out
+    
+    }
+}
